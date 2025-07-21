cdaa410948a2c4ad1e019bf17ee0fcb7550f50a7
RAJU R P <raju.ravichandran@laderatechnology.com>
2025-07-21 04:50:46 +0000

    commit

--- assesment/webapp/controller/list.controller.js
+++ assesment/webapp/controller/list.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 
     return Controller.extend("assesment.controller.list", {
         onInit() {
-         sap.ui.core.UIComponent.getRouterFor(this).getRoute("RouteList").attachPatternMatched(this._rajunav, this);
+            sap.ui.core.UIComponent.getRouterFor(this).getRoute("RouteList").attachPatternMatched(this._rajunav, this);
         },
         // _rajunav : function(oevent) {
         //     var matid = oevent.getParameter("arguments").indexlist;
@@ -14,62 +14,84 @@ sap.ui.define([
         //     this.getView().byId("LID1").setModel(vendorview, "vendorview");
         // }
 
-        _rajunav: function(oevent) {
+        _rajunav: function (oevent) {
             this.matid = oevent.getParameter("arguments").indexlist;
             var materials = this.getView().getModel("material").oData;
             var getvendor = [];
-        
+
             for (var i = 0; i < materials.length; i++) {
                 if (materials[i].materialId === this.matid) {
-                    getvendor = materials[i].vendors; 
+                    getvendor = materials[i].vendors;
                 }
             }
-        
+
             var vendorview = new sap.ui.model.json.JSONModel(getvendor);
             this.getView().byId("LID1").setModel(vendorview, "vendorview");
         },
 
         // filter
-        Onpressfil: function(evt){
+        Onpressfil: function (evt) {
             var filt = evt.getParameter("value");
-            var ofilt  = new sap.ui.model.Filter("vendorName", "Contains", filt);
+            var ofilt = new sap.ui.model.Filter("vendorName", "Contains", filt);
             this.getView().byId("LID1").getBinding("items").filter(ofilt);
         },
 
         //sorting
-        OnSortlist (){
-          this._sortlist = !this._sortlist;
-          var osort = new sap.ui.model.Sorter("vendorName", this._sortlist );
-          this.getView().byId("LID1").getBinding("items").sort(osort);
+        OnSortlist() {
+            this._sortlist = !this._sortlist;
+            var osort = new sap.ui.model.Sorter("vendorName", this._sortlist);
+            this.getView().byId("LID1").getBinding("items").sort(osort);
         },
 
         // return to first page
-        onNavPress (){
+        onNavPress() {
             this.getOwnerComponent().getRouter().navTo("RouteProduct");
         },
-        
+
         // to add 
-        OnCreatelist () {
-         if(!this.add){
-            this.add = new sap.ui.xmlfragment("assesment.view.listcreate", this);
-            this.getView().addDependent(this.add);
-         }
-         var listone = {
-                    "vendorId" : " ",
-                    "vendorName" : " "
-         };
-          var listtwo = new sap.ui.model.json.JSONModel(listone);
-          this.getView().setModel(listtwo, "listtwo");
-        this.add.open();
+        OnCreatelist() {
+            if (!this.add) {
+                this.add = new sap.ui.xmlfragment("assesment.view.listcreate", this);
+                this.getView().addDependent(this.add);
+            }
+            var listone = {
+                "vendorId": " ",
+                "vendorName": " "
+            };
+            var listtwo = new sap.ui.model.json.JSONModel(listone);
+            this.getView().setModel(listtwo, "listtwo");
+            this.add.open();
 
         },
-        onSaveProduct () {
-        // this.getView().getModel("material").oData.push(this.getView().getModel("listtwo").oData);
-        this.getView().getModel("material").oData.find(mat=> mat.materialId === (this.matid)).vendors.push(this.getView().getModel("listtwo").oData)
-        this.getView().getModel("material").updateBindings(true);
-        this.add.close();
+        onSaveProduct() {
+            // this.getView().getModel("material").oData.push(this.getView().getModel("listtwo").oData);
+            // this.getView().getModel("material").oData.find(mat=> mat.materialId === (this.matid)).vendors.push(this.getView().getModel("listtwo").oData)
+            // this.getView().getModel("material").updateBindings(true);
+            // this.add.close();
+            // Step 1: Get the model and path
+            var oMaterialModel = this.getView().getModel("material");
+            var aMaterials = oMaterialModel.getProperty("/");
+
+            // Step 2: Find the correct material object
+            var materialObj = aMaterials.find(mat => mat.materialId === this.matid);
+
+            // Step 3: Push vendor to vendors array
+            if (materialObj) {
+                var aVendors = materialObj.vendors || [];
+                aVendors.push(this.getView().getModel("listtwo").getData());
+                materialObj.vendors = aVendors;
+
+                // Step 4: Set updated array back into model
+                oMaterialModel.setProperty("/", aMaterials);
+
+                // Step 5: Force model to update bindings
+                oMaterialModel.updateBindings(true);
+            }
+
+            // Close dialog
+            this.add.close();
         },
-        onCancelProduct () {
+        onCancelProduct() {
             this.add.close();
 
         }
